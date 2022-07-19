3375c3587d8b836ec383f4262e0c226e864c6048
Lev <rumataeston@gmail.com>
2022-07-19 21:44:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const changeTodoHandler = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const addTodoHandler = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
+
+  const deleteTodoHandler = (text) => {
+    const newTodos = todos.filter((todo) => {
+      return todo !== text;
+    });
+    setTodos(newTodos);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> React Todo App </h1>
+
+      <div className="input-wrapper">
+        <input type="text" name="todo" value={todo} placeholder="Create a new todo" onChange={changeTodoHandler} />
+        <button className="add-button" onClick={addTodoHandler}>Add</button>
+      </div>
+
+      {todos?.length > 0 ? (
+        <ul className="todo-list">
+          {todos.map((todo, index) => (
+            <div className="todo">
+              <li key={index}> {todo} </li>
+
+              <button className="delete-button" onClick={() => {deleteTodoHandler(todo)}}>Delete</button>
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 }
